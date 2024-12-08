4f8e0000a6b18fd461380a4eda2f08f2d82c1c4d
aguedamaiara <costaaguedama@gmail.com>
2024-12-08 18:00:44 -0300

    Prática 2 - Parte 1: Criando as novas telas

--- app/src/main/java/com/com/weatherapp/MainActivity.kt
+++ app/src/main/java/com/com/weatherapp/MainActivity.kt
@@ -4,21 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.com.weatherapp.ui.HomePage
 import com.com.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,44 +15,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WeatherAppTheme {
-                HomePage(onLogout = {finish()})
+                HomePage()
                 }
             }
         }
     }
 
 
-@Composable
-fun HomePage(onLogout: () -> Unit, modifier: Modifier = Modifier) {
-    val context = LocalContext.current
-
-    Column(
-        modifier = modifier
-            .padding(16.dp)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Bem-vindo(a) à Home!",
-            fontSize = 24.sp
-        )
-        Spacer(modifier = modifier.size(24.dp))
-        Button(
-            onClick = {
-               onLogout()
-            },
-            modifier = Modifier.padding(top = 16.dp)
-        ) {
-            Text("Sair")
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun HomePagePreview() {
-    WeatherAppTheme {
-        HomePage(onLogout ={})
-    }
-}
