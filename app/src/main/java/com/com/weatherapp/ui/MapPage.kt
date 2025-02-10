b1d27832444635f7720d1faed9dc3c996e89356b
aguedamaiara <costaaguedama@gmail.com>
2025-02-10 09:14:50 -0300

    Prática 4 -Parte 4: Trabalhando com a localização do usuário

--- app/src/main/java/com/com/weatherapp/ui/MapPage.kt
+++ app/src/main/java/com/com/weatherapp/ui/MapPage.kt
@@ -1,31 +1,51 @@
 package com.com.weatherapp.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.com.weatherapp.R
 import com.com.weatherapp.ui.viewmodels.MainViewModel
 
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun MapPage(viewModel: MainViewModel){
+
+    val context = LocalContext.current
+    val hasLocationPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+    }
+
     val recife = LatLng(-8.05, -34.9)
     val caruaru = LatLng(-8.27, -35.98)
     val joaopessoa = LatLng(-7.12, -34.84)
@@ -33,7 +53,10 @@ fun MapPage(viewModel: MainViewModel){
 
     GoogleMap (modifier = Modifier.fillMaxSize(),
         onMapClick = { viewModel.add("Nova cidade", location = it) },
-        cameraPositionState = camPosState) {
+        cameraPositionState = camPosState,
+        properties = MapProperties(isMyLocationEnabled = hasLocationPermission),
+        uiSettings = MapUiSettings(myLocationButtonEnabled = true)
+    ) {
 
         Marker(
             state = MarkerState(position = recife),
