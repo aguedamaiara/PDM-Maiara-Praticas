ec0974221b69dad55843b89dc5995bd47f812bb4
aguedamaiara <costaaguedama@gmail.com>
2025-02-17 07:35:22 -0300

    Prática 9 - Parte 1: Carregando imagens na lista de cidade e nas previsões climáticas

--- app/src/main/java/com/com/weatherapp/ui/HomePage.kt
+++ app/src/main/java/com/com/weatherapp/ui/HomePage.kt
@@ -28,11 +28,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 import com.com.weatherapp.R
 import com.com.weatherapp.model.Forecast
@@ -65,10 +67,11 @@ fun HomePage(viewModel: MainViewModel) {
 
         // Exibição das condições climáticas atuais
         Row(modifier = Modifier.padding(16.dp)) {
-            Icon(
-                imageVector = Icons.Filled.AccountBox,
-                contentDescription = "Localized description",
-                modifier = Modifier.size(150.dp)
+            AsyncImage( // Substitui o Icon
+                model = viewModel.city?.weather?.imgUrl,
+                modifier = Modifier.size(100.dp),
+                error = painterResource(id = R.drawable.loading),
+                contentDescription = "Imagem"
             )
             Column(modifier = Modifier.padding(start = 16.dp)) {
                 Spacer(modifier = Modifier.size(12.dp))
@@ -124,10 +127,11 @@ fun ForecastItem(
             .clickable(onClick = { onClick(forecast) }),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(
-            imageVector = Icons.Filled.LocationOn,
-            contentDescription = "Localized description",
-            modifier = Modifier.size(48.dp)
+        AsyncImage( // Substitui o Icon
+            model = forecast.imgUrl,
+            modifier = Modifier.size(40.dp),
+            error = painterResource(id = R.drawable.loading),
+            contentDescription = "Imagem"
         )
         Spacer(modifier = Modifier.size(16.dp))
         Column {
--- app/src/main/java/com/com/weatherapp/ui/ListPage.kt
+++ app/src/main/java/com/com/weatherapp/ui/ListPage.kt
@@ -22,9 +22,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.com.weatherapp.R
 import com.com.weatherapp.model.City
 import com.com.weatherapp.ui.nav.Route
 import com.com.weatherapp.ui.viewmodels.MainViewModel
@@ -72,10 +75,11 @@ fun CityItem(
             .clickable { onClick() },
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(
-            imageVector = Icons.Rounded.FavoriteBorder,
-            contentDescription = "Favorite Icon",
-            modifier = Modifier.size(24.dp)
+        AsyncImage(
+            model = city.weather?.imgUrl,
+            modifier = Modifier.size(75.dp),
+            error = painterResource(id = R.drawable.loading),
+            contentDescription = "Imagem"
         )
         Spacer(modifier = Modifier.size(12.dp))
         Column(modifier = Modifier.weight(1f)) {
